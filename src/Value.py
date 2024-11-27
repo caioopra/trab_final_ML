0b1169f87b4177893daf04ff249783d47f908308
caioopra <caioprsilva@gmail.com>
2024-11-27 20:23:10 -0300

    Implementing more operations and storing operation on Value

--- src/Value.py
+++ src/Value.py
@@ -1,28 +1,51 @@
 from typing import Union
 
+
 class Value:
     """Class that stores a single value and it's gradient. Works 'similarly' to PyTorch's tensors."""
 
-    def __init__(self, data: Union[int, float]):
+    def __init__(self, data: Union[int, float], operation: str = ""):
         self.data = data
         self.grad = 0.0
 
-    def __add__(self, other: Union[int, float, 'Value']) -> 'Value':
+        self._operation = operation
+
+    def __add__(self, other: Union[int, float, "Value"]) -> "Value":
         other = other if isinstance(other, Value) else Value(other)
 
-        output = Value(self.data + other.data)
+        output = Value(self.data + other.data, operation="+")
 
         return output
 
-    def __radd__(self, other: Union[int, float, 'Value']) -> 'Value':
+    def __radd__(self, other: Union[int, float, "Value"]) -> "Value":
         return self.__add__(other)
 
-    def __mul__(self, other: Union[int, float, 'Value']) -> 'Value':
+    def __mul__(self, other: Union[int, float, "Value"]) -> "Value":
         other = other if isinstance(other, Value) else Value(other)
 
-        output = Value(self.data * other.data)
+        output = Value(self.data * other.data, operation="*")
 
         return output
+
+    def __rmul__(self, other: Union[int, float, "Value"]) -> "Value":
+        return self.__mul__(other)
+
+    def __pow__(self, other: Union[int, float]) -> "Value":
+        is_int_or_float = isinstance(other, (int, float))
+        assert is_int_or_float
+
+        output = Value(self.data**other, operation=f"**{other}")
+
+        return output
+
+    def __sub__(self, other: Union[int, float, "Value"]) -> "Value":
+        return self + (-other)
+
+    def __rsub__(self, other: Union[int, float, "Value"]) -> "Value":
+        return self.__sub__(other)
+
+    def __neg__(self):
+        return self * -1
 
     def __repr__(self):
         return f"Value(data={self.data})"
@@ -32,5 +55,5 @@ if __name__ == "__main__":
     a = Value(1)
     b = Value(3)
 
-    print(f"a + b = {a + b}")
-    print(f"a * b = {a * b}")
+    print(f"{1+a=}, {a+1=}, {a+b=}, {a*b=}, {a-b=}, {b - 1}, {3 * b}")
+
