5a4149f2e703be093d6d006d43f41fe34fb04f83
caioopra <caioprsilva@gmail.com>
2024-11-27 20:41:12 -0300

    Basic structure for backpropagation

--- src/Value.py
+++ src/Value.py
@@ -4,16 +4,22 @@ from typing import Union
 class Value:
     """Class that stores a single value and it's gradient. Works 'similarly' to PyTorch's tensors."""
 
-    def __init__(self, data: Union[int, float], operation: str = ""):
+    def __init__(
+        self,
+        data: Union[int, float],
+        operation: str = "",
+        children: Union[tuple["Value"], tuple["Value", "Value"], tuple] = (),
+    ):
         self.data = data
         self.grad = 0.0
 
         self._operation = operation
+        self._previous = set(children)
 
     def __add__(self, other: Union[int, float, "Value"]) -> "Value":
         other = other if isinstance(other, Value) else Value(other)
 
-        output = Value(self.data + other.data, operation="+")
+        output = Value(self.data + other.data, operation="+", children=(self, other))
 
         return output
 
@@ -23,7 +29,7 @@ class Value:
     def __mul__(self, other: Union[int, float, "Value"]) -> "Value":
         other = other if isinstance(other, Value) else Value(other)
 
-        output = Value(self.data * other.data, operation="*")
+        output = Value(self.data * other.data, operation="*", children=(self, other))
 
         return output
 
@@ -34,7 +40,7 @@ class Value:
         is_int_or_float = isinstance(other, (int, float))
         assert is_int_or_float
 
-        output = Value(self.data**other, operation=f"**{other}")
+        output = Value(self.data**other, operation=f"**{other}", children=(self,))
 
         return output
 
@@ -50,10 +56,39 @@ class Value:
     def __repr__(self):
         return f"Value(data={self.data})"
 
+    def backward(self):
+        """Runs the backpropagation algorithm to calculate the gradients of the values. Based on PyTorch's autograd."""
+        topology = []
+        visited_nodes = set()
+
+        # auxiliar function to build the network topology (recursively)
+        def _build_topology(node: "Value"):
+            if node in visited_nodes:
+                topology.append(node)
+                return
+
+            visited_nodes.add(node)
+
+            for prev in node._previous:  
+                _build_topology(prev)
+
+            topology.append(node)
+
+        _build_topology(self)
+
+        self.grad = 1.0  # Start with the gradient of the output node
+        print("Built topology: ", topology)
+        # TODO: calculation of each node's gradient
+
 
 if __name__ == "__main__":
     a = Value(1)
     b = Value(3)
 
     print(f"{1+a=}, {a+1=}, {a+b=}, {a*b=}, {a-b=}, {b - 1}, {3 * b}")
+    a.backward()
+    c = a + b
+    c.backward()
+    d = c * b
+    d.backward()
 
