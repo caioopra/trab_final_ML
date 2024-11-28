5392775521ce101617edcab5d14f1a5dc21c51de
caioopra <caioprsilva@gmail.com>
2024-11-27 21:22:38 -0300

    Implementing backpropagation

--- src/Value.py
+++ src/Value.py
@@ -16,10 +16,19 @@ class Value:
         self._operation = operation
         self._previous = set(children)
 
+        self._backward = lambda: None  # default backward function
+
     def __add__(self, other: Union[int, float, "Value"]) -> "Value":
+        """Overloads the '+' operator to allow for Value + Value operations."""
         other = other if isinstance(other, Value) else Value(other)
 
         output = Value(self.data + other.data, operation="+", children=(self, other))
+
+        def _backward():  # backpropagation function; partial derivative of the operation
+            self.grad += 1.0 * output.grad
+            other.grad += 1.0 * output.grad
+
+        output._backward = _backward
 
         return output
 
@@ -27,9 +36,16 @@ class Value:
         return self.__add__(other)
 
     def __mul__(self, other: Union[int, float, "Value"]) -> "Value":
+        """Overloads the '*' operator to allow for Value * Value operations."""
         other = other if isinstance(other, Value) else Value(other)
 
         output = Value(self.data * other.data, operation="*", children=(self, other))
+
+        def _backward():  # partial derivative of the operation
+            self.grad += other.data * output.grad
+            other.grad += self.data * output.grad
+
+        output._backward = _backward
 
         return output
 
@@ -37,10 +53,17 @@ class Value:
         return self.__mul__(other)
 
     def __pow__(self, other: Union[int, float]) -> "Value":
+        """Overloads the '**' operator to allow for Value ** Value operations."""
         is_int_or_float = isinstance(other, (int, float))
         assert is_int_or_float
 
         output = Value(self.data**other, operation=f"**{other}", children=(self,))
+
+        def _backward():
+            self.grad += other * self.data ** (other - 1)
+            self.grad *= output.grad
+
+        output._backward = _backward
 
         return output
 
@@ -53,15 +76,16 @@ class Value:
     def __neg__(self):
         return self * -1
 
-    def __repr__(self):
-        return f"Value(data={self.data})"
+    def __repr__(self) -> str:
+        """Returns a string representation of the Value object."""
+        return f"Value(data={self.data}, grad={self.grad})"
 
     def backward(self):
         """Runs the backpropagation algorithm to calculate the gradients of the values. Based on PyTorch's autograd."""
         topology = []
         visited_nodes = set()
 
-        # auxiliar function to build the network topology (recursively)
+        # auxiliary function to build the network topology (recursively)
         def _build_topology(node: "Value"):
             if node in visited_nodes:
                 topology.append(node)
@@ -69,7 +93,7 @@ class Value:
 
             visited_nodes.add(node)
 
-            for prev in node._previous:  
+            for prev in node._previous:
                 _build_topology(prev)
 
             topology.append(node)
@@ -77,18 +101,22 @@ class Value:
         _build_topology(self)
 
         self.grad = 1.0  # Start with the gradient of the output node
-        print("Built topology: ", topology)
-        # TODO: calculation of each node's gradient
+
+        # run the backpropagation calculatios
+        for node in topology[::-1]:
+            node._backward()
+
+        self._last_topology = topology  # TODO: remove
 
 
 if __name__ == "__main__":
     a = Value(1)
-    b = Value(3)
+    b = Value(2)
 
-    print(f"{1+a=}, {a+1=}, {a+b=}, {a*b=}, {a-b=}, {b - 1}, {3 * b}")
-    a.backward()
     c = a + b
-    c.backward()
     d = c * b
-    d.backward()
+    e = d**2
+    e.backward()
+    [print(node) for node in [a, b, c, d, e]]
+    print(e._last_topology)  # TODO: remove
 
