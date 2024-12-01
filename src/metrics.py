ec0483c54d25421a55e520fe0262a29d08510dd9
Daniel <josedanielalves018@gmail.com>
2024-12-01 16:57:22 -0300

    Root Mean Squared Error (RMSE) function implemented

--- src/metrics.py
+++ src/metrics.py
@@ -25,3 +25,15 @@ def mse(y_true: List[float], y_pred: List[float]) -> Value:
     ) / len(y_true)
 
     return Value(mse_val)
+
+
+def rmse(y_true: List[float], y_pred: List[float]) -> Value:
+    """
+    Calculates root mean squared error, given two lists of values
+    """
+    rmse_val = (
+        sum(((y - y_hat) ** 2 for y, y_hat in zip(y_true, y_pred)), Value(0.0))
+        / len(y_true)
+    ) ** (0.5)
+
+    return Value(rmse_val)
