d736f34be565fcca6af659fd2e7815e592ea3402
Daniel <josedanielalves018@gmail.com>
2024-12-01 16:54:39 -0300

    Mean Squared Error (MSE) function implemented

--- src/metrics.py
+++ src/metrics.py
@@ -14,3 +14,14 @@ def categorical_cross_entropy(y_true: List[float], y_pred: List[float]) -> Value
     loss = -sum((y * log(y_hat) for y, y_hat in zip(y_true, y_pred)))
     
     return Value(loss)
+
+
+def mse(y_true: List[float], y_pred: List[float]) -> Value:
+    """
+    Calculates mean squared error, given two lists of values
+    """
+    mse_val = sum(
+        ((y - y_hat) ** 2 for y, y_hat in zip(y_true, y_pred)), Value(0.0)
+    ) / len(y_true)
+
+    return Value(mse_val)
