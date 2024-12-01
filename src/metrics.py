ae6bd21482c8a6e18ec5ce3307080a271fee1fd2
Daniel <josedanielalves018@gmail.com>
2024-12-01 16:58:13 -0300

    Sum of Squared Errors (SSE) function implemented

--- src/metrics.py
+++ src/metrics.py
@@ -37,3 +37,10 @@ def rmse(y_true: List[float], y_pred: List[float]) -> Value:
     ) ** (0.5)
 
     return Value(rmse_val)
+
+
+def sse(y_true: List[float], y_pred: List[float]) -> Value:
+    """
+    Calculates sum of squared errors, given two lists of values
+    """
+    return sum(((yout - ygt) ** 2 for ygt, yout in zip(y_true, y_pred)), Value(0.0))
